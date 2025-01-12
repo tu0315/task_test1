d3bfb4b2488d7ac8fce23d99bd7020316d440188
tu0315 <takurou.usuki@gmail.com>
2025-01-12 22:02:04 +0900

    エロクアントとクエリビルダ

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,13 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use SebastianBergmann\CodeCoverage\Report\Xml\Tests;
 use App\Models\Test;
+use Illuminate\Support\Facades\DB; 
 
 class TestController extends Controller
 {
     public function index()
     {
+        // エロクアント
         $values = Test::all();
-        // dd($values);
+        
+        $count = Test::count();
+
+        $first = Test::findOrFail(1);
+
+        $where = Test::where("text", "=", "aaa")->get();
+        
+        // クエリビルダ
+        $queryBuilder = DB::table('tests')->where('text', '=', 'aaa')->select('id', 'text')->get();
+        
+        dd($values, $count, $first, $where, $queryBuilder);
+
         return view("tests.test", compact('values'));
     }
 }
