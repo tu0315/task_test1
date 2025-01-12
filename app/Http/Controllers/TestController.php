99ffb50645fe98fc3436d498c81d103084de1fbb
tu0315 <takurou.usuki@gmail.com>
2025-01-12 18:18:16 +0900

    MVCモデルの記述方法

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use SebastianBergmann\CodeCoverage\Report\Xml\Tests;
+use App\Models\Test;
 
 class TestController extends Controller
 {
-    //
+    public function index()
+    {
+        $values = Test::all();
+        // dd($values);
+        return view("tests.test", compact('values'));
+    }
 }
