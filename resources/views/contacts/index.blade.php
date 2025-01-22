62e85a2a18bdab52b601712400d55495a414940f
tu0315 <takurou.usuki@gmail.com>
2025-01-22 15:46:11 +0900

    index画面

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            お問い合わせ一覧
+            問い合わせ一覧
         </h2>
     </x-slot>
 
@@ -11,6 +11,28 @@
                 <div class="p-6 text-gray-900">
                     インデックス<br>
                     <a href="{{ route('contacts.create') }}" class="text-blue-500">新規登録</a>
+                    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+                        <table class="table-auto w-full text-left whitespace-no-wrap">
+                        <thead>
+                            <tr>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">ID</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">氏名</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">件名</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">登録日</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($contacts as $contact)
+                            <tr>
+                                <td class="border-t-2 border-gray-200 px-4 py-3">{{ $contact->id }}</td>
+                                <td class="border-t-2 border-gray-200 px-4 py-3">{{ $contact->name }}</td>
+                                <td class="border-t-2 border-gray-200 px-4 py-3">{{ $contact->title }}</td>
+                                <td class="border-t-2 border-gray-200 px-4 py-3">{{ $contact->created_at }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
